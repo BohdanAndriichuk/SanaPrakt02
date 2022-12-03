c372c9c4eb8e8eff290d8426fabc6012c0b32b60
Bohdan Andriichuk <sana22_abv@student.ztu.edu.ua>
2022-12-03 11:58:40 +0200

    branch text

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine($"Перемкнулися на гілку \"text\"");
+Console.WriteLine("git branch text");
+Console.WriteLine("git checkout text");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
