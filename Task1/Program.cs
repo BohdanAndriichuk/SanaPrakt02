c0bd8e3cc8de04b4a0176b43515f7c2354d54e5e
Bohdan Andriichuk <sana22_abv@student.ztu.edu.ua>
2022-12-03 12:02:10 +0200

    Text_2

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,5 +2,5 @@
 Console.WriteLine($"Перемкнулися на гілку \"text\"");
 Console.WriteLine("git branch text");
 Console.WriteLine("git checkout text");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Змінили текст повідомлення і зробили комміт");
+Console.WriteLine("git add -u && git commit -m \"name\"");
